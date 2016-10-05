4d5718239be0807883c9126ceda13347015c8df8
Frederik Sachser <f.sachser@gmx.at>
2016-10-05 12:39:42 +0200

    init commit

---
+++ R/add_text_to_vector.R
@@ -0,0 +1,26 @@
+#' add text to a vector
+#'
+#' @description This function will add predefined text to a specified vector.
+#'   The special aspect is that it will not repeat the text if it exists.
+#' @param the_vector Specify a vector that should be edited.
+#' @param the_text Character. Specify the text you want to add to your vector.
+#' @param override_NA Logical. Choose TRUE if you want to override NA values.
+#' @param sep Character. Specify the separator between the text.
+#' @author Frederik Sachser
+#' @note The idea behind the function is that you can specify a subset of a data frame and add values to it, without overriding existing values. It is meant to prepare a data frame before calling badval_NA.
+#' @seealso \code{\link{badval_NA}}
+#' @export
+
+add_text_to_vector <-
+    function(the_vector,
+             the_text,
+             override_NA = TRUE,
+             sep = ", ") {
+        the_vector[!is.na(the_vector)][-which(the_vector[!is.na(the_vector)] == the_text)] <-
+            paste0(the_vector[!is.na(the_vector)][-which(the_vector[!is.na(the_vector)] == the_text)], paste0(sep, the_text))
+
+        if (override_NA == TRUE) {
+            the_vector[is.na(the_vector)] <- the_text
+        }
+        return(the_vector)
+    }
---
+++ R/badval_NA.R
@@ -0,0 +1,27 @@
+#' Remove bad values from data
+#'
+#' @description This function will remove cells of a data frame, that have bad
+#'   values.
+#' @param data A data frame.
+#' @param bad_values Character. A Vector of the same length as nrow(data).
+#'   Generally it is meant to be a column of the data where you can specify the
+#'   names of columns that has a bad value in this row. Therefore you do not need to
+#'   delete the cells with bad values in a data frame but specify them in a
+#'   column. If there are multiple bad values per row the column names should be
+#'   separated by comma.
+#' @author Frederik Sachser
+#' @seealso \code{\link{add_text_to_vector}}
+#' @export
+
+badval_NA <- function(data, bad_values) {
+    thenames <- names(data)
+    data_out <- data
+    for (i in seq_along(thenames)) {
+        thebad <- grep(pattern = thenames[i], x = bad_values)
+        if (nrow(data[thebad, i]) > 0) {
+            data_out[thebad, i] <- NA
+        }
+    }
+    return(data_out)
+}
+
